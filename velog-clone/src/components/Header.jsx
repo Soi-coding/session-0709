09865d68978941cc340887e2d1f55d10dd845f26
Soi-coding <2023112594@dgu.ac.kr>
2025-07-15 13:00:55 +0000

    수정: 헤더, 네비게이션, 홈페이지

--- velog-clone/src/components/Header.jsx
+++ velog-clone/src/components/Header.jsx
@@ -1,38 +1,48 @@
+import React from "react";
 import velogLogo from "../assets/images/velog.png";
 import bellIcon from "../assets/images/bell.png";
 import searchIcon from "../assets/images/search.png";
 
 function Header() {
   return (
-      <header
+      <div
             style={{
-                    display: "flex",
-                            justifyContent: "space-between",
-                                    alignItems: "center",
-                                            padding: "16px 24px",
-                                                    // borderBottom 제거!
-                                                          }}
-                                                              >
-                                                                    <img src={velogLogo} alt="velog logo" style={{ height: "24px" }} />
-                                                                          <div style={{ display: "flex", alignItems: "center", gap: "16px" }}>
-                                                                                  <img src={bellIcon} alt="bell" style={{ width: "24px", height: "24px" }} />
-                                                                                          <img src={searchIcon} alt="search" style={{ width: "24px", height: "24px" }} />
-                                                                                                  <button
-                                                                                                            style={{
-                                                                                                                        backgroundColor: "#000",
-                                                                                                                                    color: "#fff",
-                                                                                                                                                border: "none",
-                                                                                                                                                            borderRadius: "999px",
-                                                                                                                                                                        padding: "8px 16px",
-                                                                                                                                                                                    fontWeight: "bold",
-                                                                                                                                                                                                cursor: "pointer",
-                                                                                                                                                                                                          }}
-                                                                                                                                                                                                                  >
-                                                                                                                                                                                                                            로그인
-                                                                                                                                                                                                                                    </button>
-                                                                                                                                                                                                                                          </div>
-                                                                                                                                                                                                                                              </header>
-                                                                                                                                                                                                                                                );
-                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                export default Header;
+                    maxWidth: "1200px",
+                            margin: "0 auto",
+                                    padding: "0 20px",
+                                          }}
+                                              >
+                                                    <header
+                                                            style={{
+                                                                      display: "flex",
+                                                                                justifyContent: "space-between",
+                                                                                          alignItems: "center",
+                                                                                                    padding: "16px 0", // 좌우 padding은 바깥 div에
+                                                                                                              borderBottom: "1px solid #ddd",
+                                                                                                                      }}
+                                                                                                                            >
+                                                                                                                                    <img src={velogLogo} alt="velog logo" style={{ height: "24px" }} />
+                                                                                                                                            <div style={{ display: "flex", alignItems: "center", gap: "16px" }}>
+                                                                                                                                                      <img src={bellIcon} alt="bell" style={{ width: "24px", height: "24px" }} />
+                                                                                                                                                                <img src={searchIcon} alt="search" style={{ width: "24px", height: "24px" }} />
+                                                                                                                                                                          <button
+                                                                                                                                                                                      style={{
+                                                                                                                                                                                                    backgroundColor: "#000",
+                                                                                                                                                                                                                  color: "#fff",
+                                                                                                                                                                                                                                border: "none",
+                                                                                                                                                                                                                                              borderRadius: "999px", 
+                                                                                                                                                                                                                                                            padding: "8px 16px",
+                                                                                                                                                                                                                                                                          fontWeight: "bold", 
+                                                                                                                                                                                                                                                                                        cursor: "pointer",
+                                                                                                                                                                                                                                                                                                    }}
+                                                                                                                                                                                                                                                                                                              >
+                                                                                                                                                                                                                                                                                                                          로그인
+                                                                                                                                                                                                                                                                                                                                    </button>
+                                                                                                                                                                                                                                                                                                                                            </div>
+                                                                                                                                                                                                                                                                                                                                                  </header>
+                                                                                                                                                                                                                                                                                                                                                      </div>
+                                                                                                                                                                                                                                                                                                                                                        );
+                                                                                                                                                                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                                                                                                                                                        export default Header;
+                                                                                                                                                                                                                                                                                                                                                        
